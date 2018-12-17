af203a35c1cc18e492fda3ec9fc0229863692d51
Mark Carter <alt.mcarter@gmail.com>
2018-12-17 16:41:39 +0000

    minitone improvements

--- minitone85/minitone85.ino
+++ minitone85/minitone85.ino
@@ -3,14 +3,16 @@
 void setup() {
   CLKPR = 0x80;
   CLKPR = 0;
-  pinMode(PB1, OUTPUT);  
-  miniTone(5000); // 5kHz
-  // TCCR1 = 0x90 // stop the counter
-
+  pinMode(PB1, OUTPUT);    
 }
 
 void miniTone(unsigned long freq)
 {
+  if(freq==0) {
+    TCCR1 = 0x90; // stop the counter
+    return;
+  }
+
   freq *= 2;
   int prescale = 1;
   while(freq * 256 * (1<<(prescale-1)) < F_CPU)
@@ -20,6 +22,14 @@ void miniTone(unsigned long freq)
   OCR1C = divisor;         // set the OCR
 }
 
+void chime(unsigned long freq, unsigned long duration) {
+  miniTone(freq);
+  delay(duration);
+}
 void loop()
 {
+  chime(2200, 500);
+  chime(0, 500);
+  chime(1100, 500);
+  chime(0, 1500);  
 }
--- om/om.ino
+++ om/om.ino
@@ -1,7 +1,7 @@
 /*
  * Started 20-Jan-2018
  * 
- * Meditation timer
+ * Meditation timer and white noise generator
  * 
  * You probably need the clock running at 8MHz
  * 
@@ -10,8 +10,8 @@
 
 #include <Arduino.h>
 #if defined(__AVR_ATtiny85__)  
-#define F_CPU 8'000'000UL
-constexpr int buzzer = PB0;
+#define F_CPU 8000000UL
+constexpr int buzzer = PB1;
 constexpr int btn = PB4;
 //static_assert(0, "X");
 #else
@@ -53,6 +53,22 @@ bool pdelay(ms_t ms) { // true => completed, flase => interrupted
   return true;
 }
 
+void miniTone(unsigned long freq)
+{
+  if(freq==0) {
+    TCCR1 = 0x90; // stop the counter
+    return;
+  }
+  
+  freq *= 2;
+  int prescale = 1;
+  while(freq * 256 * (1<<(prescale-1)) < F_CPU)
+    prescale++;
+  TCCR1 = 0x90 | prescale;
+  int divisor = F_CPU/(1<<(prescale-1))/freq;
+  OCR1C = divisor;         // set the OCR
+}
+
 void setup()
 {
 #if defined(__AVR_ATtiny85__) 
@@ -67,15 +83,13 @@ void setup()
   //Serial.begin(9600);
 }
 
+bool toner(ms_t freq, ms_t duration) {
+  miniTone(freq);
+  return pdelay(duration);
+}
 void beep(int postDelay)
 {
-  /*
-  analogWrite(buzzer, 210); // about the right volume. 150 is too low. 
-  delay(500);
-  digitalWrite(buzzer, LOW);
-  delay(postDelay);
-  */
-  (tone(buzzer, 750), pdelay(500)) && (noTone(buzzer), pdelay(postDelay));
+  toner(750, 500) && toner(0, postDelay);
 }
 void beepbeep(int postDelay)
 {
@@ -106,7 +120,8 @@ void loop()
 	  case WHITE:
 	    //noTone(buzzer);
 	    delayMicroseconds(100);
-	    digitalWrite(buzzer, random(2));
+	    miniTone(random(400, 10000));
+	    //digitalWrite(buzzer, random(2));
 	    break;
 	  case START_TIMER:
 	    startTime = timeNow;
