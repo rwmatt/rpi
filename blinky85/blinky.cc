d87b90a9580e4a2c646399cb64b8478b48756450
Mark Carter <alt.mcarter@gmail.com>
2018-12-21 13:47:24 +0000

    Indented code

--- blinky85/blinky.cc
+++ blinky85/blinky.cc
@@ -4,12 +4,13 @@
 
 int main(void)
 {
-DDRB = 0xFF; // PORTB is output, all pins
-PORTB = 0x00; // Make pins low to start
+	DDRB = 0xFF;			// PORTB is output, all pins
+	PORTB = 0x00;			// Make pins low to start
 
-for (;;) {
-PORTB ^= 0xFF; // invert all the pins
-_delay_ms(10000); // wait some time
-}
-return 0;
+	for (;;)
+	{
+		PORTB ^= 0xFF;		// invert all the pins
+		_delay_ms (10000);	// wait some time
+	}
+	return 0;
 }
