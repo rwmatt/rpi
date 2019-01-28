4aeadff78ffe95aa91bea941cd0f4b3ce71187a7
Mark Carter <alt.mcarter@gmail.com>
2019-01-28 12:45:35 +0000

    new file:   clock.ino

---
+++ clock/clock/clock.ino
@@ -0,0 +1,33 @@
+
+#include <Wire.h>
+#include "RTClib.h"
+RTC_DS1307 RTC;
+
+void setup () {
+    Serial.begin(9600);
+    Wire.begin();
+    RTC.begin();
+    bool reset = ! RTC.isrunning();
+    reset = 0;
+    if (reset) {
+      Serial.println("RTC is NOT running!");
+      // following line sets the RTC to the date & time this sketch was compiled
+      RTC.adjust(DateTime(__DATE__, __TIME__));
+      }
+}
+void loop () {
+    DateTime now = RTC.now(); 
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(' ');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println(); 
+    delay(1000);
+}
