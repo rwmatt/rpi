1b8f1e6b629aa926d28d40d691f84a7901e4ba95
Mark Carter <alt.mcarter@gmail.com>
2019-05-13 20:42:37 +0100

    mal now include Every class

--- mal/mal.cpp
+++ mal/mal.cpp
@@ -1,3 +1,11 @@
 #include "mal.h"
 
 int life() { return 42; }
+
+bool Every::rising() { 
+    unsigned long now = millis();
+    if(now<start) { start = now; } // timer overflow
+    if(now-start < interval) return false;
+    start = now;    
+    return true; 
+}
--- mal/mal.h
+++ mal/mal.h
@@ -1,4 +1,14 @@
 #pragma once
+#include <Arduino.h>
 
 int life();
 
+
+class Every {
+  public:
+  Every(unsigned long interval) : interval(interval) {}
+  bool rising();
+  private:
+  unsigned long interval, start = millis();
+  
+};
