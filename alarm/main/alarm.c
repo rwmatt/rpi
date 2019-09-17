ff041116bb77e37a9736965d5fa49a57276492f1
Mark Carter <alt.mcarter@gmail.com>
2019-09-17 15:23:20 +0100

    Improved alarm

--- alarm/main/alarm.c
+++ alarm/main/alarm.c
@@ -24,22 +24,6 @@
 #include <lwip/netdb.h>
 
 
-/* The examples use simple WiFi configuration that you can set via
-   'make menuconfig'.
-   If you'd rather not, just change the below entries to strings with
-   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
-*/
-/*
-#define EXAMPLE_WIFI_SSID CONFIG_WIFI_SSID
-#define EXAMPLE_WIFI_PASS CONFIG_WIFI_PASSWORD
-
-#ifdef CONFIG_EXAMPLE_IPV4
-#define HOST_IP_ADDR CONFIG_EXAMPLE_IPV4_ADDR
-#else
-#define HOST_IP_ADDR CONFIG_EXAMPLE_IPV6_ADDR
-#endif
-*/
-
 #include "/home/mcarter/repos/redact/docs/secret/settings.h"
 
 #define PORT 3333
@@ -54,7 +38,6 @@ const int IPV6_GOTIP_BIT = BIT1;
 #endif
 
 static const char *TAG = "alarm";
-static const char *payload = "Message from ESP32 ";
 
 int secs(int n) { return n * 1000 / portTICK_PERIOD_MS;}
 #define DELAY_S(n) vTaskDelay(secs(n))
@@ -221,77 +204,6 @@ finis:
 	}
     
 }
-static void tcp_client_task(void *pvParameters)
-{
-    char rx_buffer[128];
-    char addr_str[128];
-    int addr_family;
-    int ip_protocol;
-
-    while (1) {
-
-#ifdef CONFIG_EXAMPLE_IPV4
-        struct sockaddr_in destAddr;
-        destAddr.sin_addr.s_addr = inet_addr(HOST_IP_ADDR);
-        destAddr.sin_family = AF_INET;
-        destAddr.sin_port = htons(PORT);
-        addr_family = AF_INET;
-        ip_protocol = IPPROTO_IP;
-        inet_ntoa_r(destAddr.sin_addr, addr_str, sizeof(addr_str) - 1);
-#else // IPV6
-        struct sockaddr_in6 destAddr;
-        inet6_aton(HOST_IP_ADDR, &destAddr.sin6_addr);
-        destAddr.sin6_family = AF_INET6;
-        destAddr.sin6_port = htons(PORT);
-        addr_family = AF_INET6;
-        ip_protocol = IPPROTO_IPV6;
-        inet6_ntoa_r(destAddr.sin6_addr, addr_str, sizeof(addr_str) - 1);
-#endif
-
-        int sock =  socket(addr_family, SOCK_STREAM, ip_protocol);
-        if (sock < 0) {
-            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
-            break;
-        }
-        ESP_LOGI(TAG, "Socket created");
-
-        int err = connect(sock, (struct sockaddr *)&destAddr, sizeof(destAddr));
-        if (err != 0) {
-            ESP_LOGE(TAG, "Socket unable to connect: errno %d", errno);
-        }
-        ESP_LOGI(TAG, "Successfully connected");
-
-        while (1) {
-            int err = send(sock, payload, strlen(payload), 0);
-            if (err < 0) {
-                ESP_LOGE(TAG, "Error occured during sending: errno %d", errno);
-                break;
-            }
-
-            int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
-            // Error occured during receiving
-            if (len < 0) {
-                ESP_LOGE(TAG, "recv failed: errno %d", errno);
-                break;
-            }
-            // Data received
-            else {
-                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
-                ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
-                ESP_LOGI(TAG, "%s", rx_buffer);
-            }
-
-            vTaskDelay(2000 / portTICK_PERIOD_MS);
-        }
-
-        if (sock != -1) {
-            ESP_LOGE(TAG, "Shutting down socket and restarting...");
-            shutdown(sock, 0);
-            close(sock);
-        }
-    }
-    vTaskDelete(NULL);
-}
 
 void app_main()
 {
@@ -304,6 +216,5 @@ void app_main()
     initialise_wifi();
     wait_for_ip();
 
-    //xTaskCreate(tcp_client_task, "tcp_client", 4096, NULL, 5, NULL);
-    xTaskCreate(button_task, "button_yask", 4096, NULL, 5, NULL);
+    xTaskCreate(button_task, "button_task", 4096, NULL, 5, NULL);
 }
