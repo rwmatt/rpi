f15275914b7f7dd7d759090355f72bf1a81e362f
Mark Carter <alt.mcarter@gmail.com>
2019-11-19 16:36:55 +0000

    C pretty formatting

--- i2s/sow/sow32/main/main.c
+++ i2s/sow/sow32/main/main.c
@@ -5,7 +5,7 @@
    Unless required by applicable law or agreed to in writing, this
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
-*/
+   */
 #include <string.h>
 #include <sys/param.h>
 #include "freertos/FreeRTOS.h"
@@ -39,86 +39,86 @@ static const char *payload = "Message from ESP32 ";
 
 static void udp_client_task(void *pvParameters)
 {
-    char rx_buffer[128];
-    char addr_str[128];
-    int addr_family;
-    int ip_protocol;
+	char rx_buffer[128];
+	char addr_str[128];
+	int addr_family;
+	int ip_protocol;
 
-    while (1) {
+	while (1) {
 
 #ifdef CONFIG_EXAMPLE_IPV4
-        struct sockaddr_in dest_addr;
-        dest_addr.sin_addr.s_addr = inet_addr(HOST_IP_ADDR);
-        dest_addr.sin_family = AF_INET;
-        dest_addr.sin_port = htons(PORT);
-        addr_family = AF_INET;
-        ip_protocol = IPPROTO_IP;
-        inet_ntoa_r(dest_addr.sin_addr, addr_str, sizeof(addr_str) - 1);
+		struct sockaddr_in dest_addr;
+		dest_addr.sin_addr.s_addr = inet_addr(HOST_IP_ADDR);
+		dest_addr.sin_family = AF_INET;
+		dest_addr.sin_port = htons(PORT);
+		addr_family = AF_INET;
+		ip_protocol = IPPROTO_IP;
+		inet_ntoa_r(dest_addr.sin_addr, addr_str, sizeof(addr_str) - 1);
 #else // IPV6
-        struct sockaddr_in6 dest_addr;
-        inet6_aton(HOST_IP_ADDR, &dest_addr.sin6_addr);
-        dest_addr.sin6_family = AF_INET6;
-        dest_addr.sin6_port = htons(PORT);
-        addr_family = AF_INET6;
-        ip_protocol = IPPROTO_IPV6;
-        inet6_ntoa_r(dest_addr.sin6_addr, addr_str, sizeof(addr_str) - 1);
+		struct sockaddr_in6 dest_addr;
+		inet6_aton(HOST_IP_ADDR, &dest_addr.sin6_addr);
+		dest_addr.sin6_family = AF_INET6;
+		dest_addr.sin6_port = htons(PORT);
+		addr_family = AF_INET6;
+		ip_protocol = IPPROTO_IPV6;
+		inet6_ntoa_r(dest_addr.sin6_addr, addr_str, sizeof(addr_str) - 1);
 #endif
 
-        int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
-        if (sock < 0) {
-            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
-            break;
-        }
-        ESP_LOGI(TAG, "Socket created, sending to %s:%d", HOST_IP_ADDR, PORT);
+		int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
+		if (sock < 0) {
+			ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+			break;
+		}
+		ESP_LOGI(TAG, "Socket created, sending to %s:%d", HOST_IP_ADDR, PORT);
 
-        while (1) {
+		while (1) {
 
-            int err = sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-            if (err < 0) {
-                ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
-                break;
-            }
-            ESP_LOGI(TAG, "Message sent");
+			int err = sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+			if (err < 0) {
+				ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+				break;
+			}
+			ESP_LOGI(TAG, "Message sent");
 
-            struct sockaddr_in source_addr; // Large enough for both IPv4 or IPv6
-            socklen_t socklen = sizeof(source_addr);
-            int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *)&source_addr, &socklen);
+			struct sockaddr_in source_addr; // Large enough for both IPv4 or IPv6
+			socklen_t socklen = sizeof(source_addr);
+			int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *)&source_addr, &socklen);
 
-            // Error occurred during receiving
-            if (len < 0) {
-                ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
-                break;
-            }
-            // Data received
-            else {
-                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
-                ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
-                ESP_LOGI(TAG, "%s", rx_buffer);
-            }
+			// Error occurred during receiving
+			if (len < 0) {
+				ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
+				break;
+			}
+			// Data received
+			else {
+				rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
+				ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
+				ESP_LOGI(TAG, "%s", rx_buffer);
+			}
 
-            vTaskDelay(2000 / portTICK_PERIOD_MS);
-        }
+			vTaskDelay(2000 / portTICK_PERIOD_MS);
+		}
 
-        if (sock != -1) {
-            ESP_LOGE(TAG, "Shutting down socket and restarting...");
-            shutdown(sock, 0);
-            close(sock);
-        }
-    }
-    vTaskDelete(NULL);
+		if (sock != -1) {
+			ESP_LOGE(TAG, "Shutting down socket and restarting...");
+			shutdown(sock, 0);
+			close(sock);
+		}
+	}
+	vTaskDelete(NULL);
 }
 
 void app_main()
 {
-    ESP_ERROR_CHECK(nvs_flash_init());
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+	ESP_ERROR_CHECK(nvs_flash_init());
+	tcpip_adapter_init();
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
-    ESP_ERROR_CHECK(example_connect());
+	/* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+	 * Read "Establishing Wi-Fi or Ethernet Connection" section in
+	 * examples/protocols/README.md for more information about this function.
+	 */
+	ESP_ERROR_CHECK(example_connect());
 
-    xTaskCreate(udp_client_task, "udp_client", 4096, NULL, 5, NULL);
+	xTaskCreate(udp_client_task, "udp_client", 4096, NULL, 5, NULL);
 }
